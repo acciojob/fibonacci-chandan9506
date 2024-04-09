9e36c2bc3d7b302ccc4c02501fca21d16ed6a746
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-09 14:25:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+
+	 let fib = [0, 1];
+
+    for (let i = 2; i <= num; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib[num];
 }
 
 module.exports = fibonacci;
